7d8d8918497af8ace4e43b0cebc1a425425085c0
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-13 00:42:00 +0700

    catatan-catatan

--- 5-vue/1-vue-introduction/1-vue-belademy/directive/app.js
+++ 5-vue/1-vue-introduction/1-vue-belademy/directive/app.js
@@ -9,12 +9,7 @@ let no = '00'
 const app =  Vue.createApp({
     data(){
         return {
-            no : 01,
-            nama: 'test langsung',
-            human,
-            isTrue : true,
-            qty : 1,
-            sebuahArray : [2,3,4,5,7,21,23,2323 ]
+            title : 'Tutor Directive'
         };
     },
 
@@ -41,23 +36,4 @@ const app =  Vue.createApp({
     }
 });
 app.mount('#app')
-
-console.log(app._component.data().no) //* cara akses data melalui console.log ? tp cara lebih pendeknya gimana ?
-
-
-
-//? Properti-properti pad ainstance Vue.createApp({})
-//? memiliki template
-//! template: '<h1>mantap</h1>',
-
-//!
-
-
-//* Mungkin ini gaya penulisan vue 2 //* property ini berubah jadi fungsi pada vue 3
-//* tidak direkomendasikan karena tidak memiliki reactive capability
-//! el: '#app', //* penghubung ke element //!pada vue 3 tidak ada lagi el 
-//! Bahkan tidak didukung lagi
-    // data : {
-    //     test : 'woy'
-    // },
 
---
+++ 5-vue/1-vue-introduction/1-vue-belademy/directive/readme.md
@@ -0,0 +1,12 @@
+Directive
+
+1. fungsi-fungsi yang disediakan vue js untuk ditempelkan pada elemen html
+2. contoh :
+    1. v-one
+    2. v-if
+    3. v-else
+    4. v-show
+3. directive mirip attribut pada html namun diberikan oleh vue.js
+
+v-text -> 
+v-html ->
