30b413902d13ec3daf97df1110f7ad91e23d8398
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-13 00:02:08 +0700

    binding expression

--- 5-vue/1-vue-introduction/1-vue-belademy/basic/app.js
+++ 5-vue/1-vue-introduction/1-vue-belademy/basic/app.js
@@ -1,11 +1,40 @@
 //* disimpan dalam variable bisa disimpan dalam variable
  //* versi 2 bisa new Vue
-const app =  Vue.createApp({
-    template: '<h1>mantap</h1>'
-})
-// el: '#app', //* penghubung ke element //!pada vue 3 tidak ada lagi el 
-console.log('sebelum mount')
-app.mount('#app')
-console.log('setelah mount')
-console.log(app.$el)
 
+const human = {
+    nama : "alfonsus",
+    umur : 20
+} 
+let no = '00'
+const app =  Vue.createApp({
+    data(){
+        return {
+            no : 01,
+            nama: 'test langsung',
+            human,
+        };
+    },
+});
+app.mount('#app')
+
+console.log(app._component.data().no) //* cara akses data melalui console.log ? tp cara lebih pendeknya gimana ?
+
+
+
+
+//? Properti-properti pad ainstance Vue.createApp({})
+//? memiliki template
+//! template: '<h1>mantap</h1>',
+
+//!
+
+
+//* Mungkin ini gaya penulisan vue 2 //* property ini berubah jadi fungsi pada vue 3
+//* tidak direkomendasikan karena tidak memiliki reactive capability
+//! el: '#app', //* penghubung ke element //!pada vue 3 tidak ada lagi el 
+//! Bahkan tidak didukung lagi
+    // data : {
+    //     test : 'woy'
+    // },
+
+    {{ ini namanya binding expression}}
