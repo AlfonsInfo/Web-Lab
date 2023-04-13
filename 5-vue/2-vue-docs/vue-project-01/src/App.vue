c3e5fbd09d454555abc3dd8f19f3c60094e72cec
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-14 01:21:06 +0700

    belajar vue

--- 5-vue/2-vue-docs/vue-project-01/src/App.vue
+++ 5-vue/2-vue-docs/vue-project-01/src/App.vue
@@ -1,61 +1,22 @@
 <template>
-  <v-app>
-
-    <v-card
-    height="400"
-    width="256"
+  <v-card
     class="mx-auto"
+    max-width="300"
   >
-    <v-navigation-drawer permanent>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      
-      <v-divider></v-divider>
-      
-      <v-list
-        dense
-        nav
-        >
-        <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        link
-        >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-        
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
-</v-card>
-</v-app>
+    <v-list :items="items"></v-list>
+  </v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
   export default {
-    data () {
-      return {
-        items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
-        ],
-        right: null,
-      }
-    },
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+      on : false
+    }),
   }
 </script>
