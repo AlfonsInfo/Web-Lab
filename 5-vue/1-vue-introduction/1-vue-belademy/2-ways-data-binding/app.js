c53b6cde1d68d269a10ae2cc8f2cbab6b94f39a8
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-13 11:14:03 +0700

    2 ways data binding

---
+++ 5-vue/1-vue-introduction/1-vue-belademy/2-ways-data-binding/app.js
@@ -0,0 +1,30 @@
+//* disimpan dalam variable bisa disimpan dalam variable
+ //* versi 2 bisa new Vue
+
+const human = {
+    nama : "alfonsus",
+    umur : 20
+} 
+let no = '00'
+const app =  Vue.createApp({
+    data(){
+        return {
+            title : 'Two Way Data Binding',
+            inputText : '',
+            // kelas : '' //* ngambil data true /falseny (isi/kosong)
+            kelas : ['python','golang','dart','kotlin'],
+            ambilKelas : []
+        };
+    },
+
+    methods : {
+        reRender(data)
+        {
+            console.log(data)
+            console.log(this.inputText)
+        }
+    },
+    }
+);
+app.mount('#app')
+
