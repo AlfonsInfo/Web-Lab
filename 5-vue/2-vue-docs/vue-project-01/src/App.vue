d76b0c9cb5188d549a2820874360c0549cdfdd28
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-13 17:36:51 +0700

    dontknow

--- 5-vue/2-vue-docs/vue-project-01/src/App.vue
+++ 5-vue/2-vue-docs/vue-project-01/src/App.vue
@@ -1,27 +1,34 @@
-<script setup>
-// import data from '../../vue-project-01/src/script'
-import "vuetify/dist/vuetify.min.css";
-// const data1 = data;
-const dataLagi= {
-  nama : 'Alfons'
-}
-
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <p></p>
-  </header>
-
-  <main>
-    <!-- <h1>{{Data }}</h1> -->
-    <!-- //* Kalo tidak pake setup tidak bisa detect nama langsung -->
-    <h1>{{ dataLagi.nama}}</h1>
-  </main>
+  <v-container
+    id="input-usage"
+    fluid
+  >
+    <v-row>
+      <v-col cols="12">
+        <v-input
+          :messages="['Messages']"
+          append-icon="mdi-close"
+          prepend-icon="mdi-phone"
+        >
+          Default Slot
+        </v-input>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-</style>
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
